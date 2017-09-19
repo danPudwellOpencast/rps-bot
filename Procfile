dbedc73ac5547316592628bd0ada8dba50e0afcc
danPudwellOpencast <dan.pudwell@opencastsoftware.com>
2017-09-19 15:45:29 +0100

    removing csrf filter and procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: target/universal/stage/bin/rock-paper-scissors-ated
--- app/controllers/MoveController.scala
+++ app/controllers/MoveController.scala
@@ -3,12 +3,19 @@ package controllers
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
+import scala.util.Random
+
 class MoveController extends Controller {
   def move() = Action {
-    Ok(Json.toJson("ROCK"))
+    Ok(Json.toJson(getMove))
   }
 
   def lastOpponentMove() = Action {
     Ok
+  }
+
+  private def getMove: String = {
+    val moves = Map(0 -> "ROCK", 1 -> "PAPER", 2 -> "SCISSORS", 3 -> "DYNAMITE", 4 -> "WATERBOMB")
+    moves(Random.nextInt(4))
   }
 }
